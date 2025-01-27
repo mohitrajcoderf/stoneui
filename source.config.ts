e3d87d92bc9bd77ca92aef46888054f9193cdcea
Mohit Raj <mohitraj12099@gmail.com>
2025-01-27 13:16:53 +0000

    initial commit update

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,21 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { rehypeCodeDefaultOptions, rehypeToc } from "fumadocs-core/mdx-plugins"
+import { defineConfig, defineDocs } from "fumadocs-mdx/config"
+
+import { rehypeComponent } from "./lib/rehype-component"
 
 export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
-});
+  dir: "content/docs",
+})
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
+      transformers: [...(rehypeCodeDefaultOptions.transformers ?? [])],
+    },
+    rehypePlugins: () => [rehypeComponent, rehypeToc],
+  },
+})
